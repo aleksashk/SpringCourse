dd29013d2bb359b5d37246168f849fdd0b2ec59f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-22 13:29:24 +0300

    AOP save Pointcut - @Pointcut("pointcut_expression") pointcut reference

--- src/main/java/com/aleksandrphilimonov/spring/aop/Test1.java
+++ src/main/java/com/aleksandrphilimonov/spring/aop/Test1.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test1 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
-        Book book = context.getBean("book", Book.class);
+//        Book book = context.getBean("book", Book.class);
 
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
-        uniLibrary.getBook(book);
-//        uniLibrary.getMagazine();
+        uniLibrary.getBook();
+        uniLibrary.getMagazine();
 
 
 
