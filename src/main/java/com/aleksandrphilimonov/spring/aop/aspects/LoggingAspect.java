929e1168df1d2b5b40359febbde3c7fa55335e80
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-22 11:37:59 +0300

    AOP introduction @After

--- src/main/java/com/aleksandrphilimonov/spring/aop/aspects/LoggingAspect.java
+++ src/main/java/com/aleksandrphilimonov/spring/aop/aspects/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.aleksandrphilimonov.spring.aop.aspects;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 import org.aspectj.lang.annotation.Aspect;
@@ -12,4 +13,11 @@ public class LoggingAspect {
     public void beforeGetBookAdvice(){
         System.out.println("beforeGetBookAdvice: попытка получить книгу");
     }
+
+    @After("execution(public void getBook())")
+    public void afterGetBookAdvice(){
+        System.out.println("afterGetBookAdvice: после попытки получить книгу");
+    }
+
+
 }
