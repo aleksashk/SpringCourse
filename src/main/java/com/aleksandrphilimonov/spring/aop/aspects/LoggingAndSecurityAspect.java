5062e74ae93bf312d2f4084f8dc9bfb61f12ec1b
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-22 13:45:51 +0300

    Pointcut combination v2

--- src/main/java/com/aleksandrphilimonov/spring/aop/aspects/LoggingAndSecurityAspect.java
+++ src/main/java/com/aleksandrphilimonov/spring/aop/aspects/LoggingAndSecurityAspect.java
@@ -9,28 +9,43 @@ import org.aspectj.lang.annotation.Aspect;
 @Aspect
 public class LoggingAndSecurityAspect {
 
-    @Pointcut("execution(* com.aleksandrphilimonov.spring.aop.UniLibrary.get* ())")
-    private void allGetMethodsFromUniLibrary(){}
+    @Pointcut("execution(* com.aleksandrphilimonov.spring.aop.UniLibrary.*(..))")
+    private void allMethodsFromUniLibrary(){}
 
-    @Pointcut("execution(* com.aleksandrphilimonov.spring.aop.UniLibrary.return* ())")
-    private void allReturnMethodsFromUniLibrary(){}
+    @Pointcut("execution(public void com.aleksandrphilimonov.spring.aop.UniLibrary.returnMagazine())")
+    private void returnMagazineFromUniLibrary(){}
 
-    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
-    private void allGetAndReturnMethodsFromUniLibrary(){}
+    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
+    private void allMethodsExceptReturnMagazineFromUniLibrary(){}
 
-    @Before("allGetMethodsFromUniLibrary()")
-    public void beforeGetLoggingAdvice(){
-        System.out.println("beforeGetLoggingAdvice: writing Log #1");
+    @Before("allMethodsExceptReturnMagazineFromUniLibrary()")
+    public void beforeAllMethodsExceptReturnMagazineAdvice(){
+        System.out.println("beforeAllMethodsExceptReturnMagazineAdvice: Log #10");
     }
 
-    @Before("allReturnMethodsFromUniLibrary()")
-    public void beforeReturnLoggingAdvice(){
-        System.out.println("beforeReturnLoggingAdvice: writing Log #2");
-    }
 
-    @Before("allGetAndReturnMethodsFromUniLibrary()")
-    public void beforeGetAndReturnLoggingAdvice(){
-        System.out.println("beforeGetAndReturnLoggingAdvice: writing Log #3");
-    }
+//    @Pointcut("execution(* com.aleksandrphilimonov.spring.aop.UniLibrary.get* ())")
+//    private void allGetMethodsFromUniLibrary(){}
+//
+//    @Pointcut("execution(* com.aleksandrphilimonov.spring.aop.UniLibrary.return* ())")
+//    private void allReturnMethodsFromUniLibrary(){}
+//
+//    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
+//    private void allGetAndReturnMethodsFromUniLibrary(){}
+//
+//    @Before("allGetMethodsFromUniLibrary()")
+//    public void beforeGetLoggingAdvice(){
+//        System.out.println("beforeGetLoggingAdvice: writing Log #1");
+//    }
+//
+//    @Before("allReturnMethodsFromUniLibrary()")
+//    public void beforeReturnLoggingAdvice(){
+//        System.out.println("beforeReturnLoggingAdvice: writing Log #2");
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromUniLibrary()")
+//    public void beforeGetAndReturnLoggingAdvice(){
+//        System.out.println("beforeGetAndReturnLoggingAdvice: writing Log #3");
+//    }
 
 }
