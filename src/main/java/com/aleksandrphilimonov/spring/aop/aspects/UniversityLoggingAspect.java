8eef129d206b3a05bf1f55fa2e5db9a291fba252
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-22 15:54:42 +0300

    @AfterReturning advice refactoring objects data

--- src/main/java/com/aleksandrphilimonov/spring/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/com/aleksandrphilimonov/spring/aop/aspects/UniversityLoggingAspect.java
@@ -1,21 +1,35 @@
 package com.aleksandrphilimonov.spring.aop.aspects;
 
+import com.aleksandrphilimonov.spring.aop.Student;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Aspect
 public class UniversityLoggingAspect {
 
     @Before("execution(* getStudents())")
-    public void beforeGetStudentsLoggingAdvice(){
+    public void beforeGetStudentsLoggingAdvice() {
         System.out.println("beforeGetStudentsLoggingAdvice: логируем получение списка студентов перед методом getStudents");
     }
 
-    @AfterReturning("execution(* getStudents())")
-    public void afterGetStudentsLoggingAdvice(){
+    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
+    public void afterGetStudentsLoggingAdvice(List<Student> students) {
+
+        Student firstStudent = students.get(0);
+
+        String fullName = firstStudent.getFullName();
+        fullName = "Mr. " + fullName;
+        firstStudent.setFullName(fullName);
+
+        double avgGrade = firstStudent.getAvgGrade();
+        avgGrade = avgGrade + 1;
+        firstStudent.setAvgGrade(avgGrade);
+
         System.out.println("afterGetStudentsLoggingAdvice: логируем получение списка студентов после работы метода getStudents");
     }
 }
